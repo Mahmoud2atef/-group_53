f42f5ca12e6ae2a7841189606a41f5bcc014d524
‪Mahmoud Atef‬‏ <112256331+Mahmoud2atef@users.noreply.github.com>
2024-06-21 07:59:15 +0300

    Add files via upload

---
+++ main.php
@@ -0,0 +1,52 @@
+<?php
+echo "<h1>gg</h1>";
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>mahmoud</h1>
+    <p> environment </p>
+    <?php echo "<h3> bebo <h3>" ;?>
+    <?php echo "bebo" . " main" ;?>
+    <br>
+    <?php echo 'ahmed\'s car' ;?>
+    
+    <?php
+    // $myname = "ahmed";
+    // echo "$myname jdsvfcxxc";
+    // echo "< br>";
+    // echo '$myname jdsvfcxxc';
+    // echo " <br>";
+//    $x= 2;
+//    $y= $x;
+//    echo $y;
+//     &
+//    $k ='ahmed';
+//     $$k ="6565";
+//     echo $ahmed ;
+//     echo "i am {$$k}";
+    // $jfdj=true ;
+    // var_dump($jfdj);
+    // var_dump($jfdj);
+
+
+
+
+
+
+?>
+
+
+<?php print "<h3> bebo from print <h3>" ;?>
+
+</body>
+</html>
